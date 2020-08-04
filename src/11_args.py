2add3c2eef67756a93e3322cc3570bc386cc3e59
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-04 15:15:31 -0500

    11 args done

--- src/11_args.py
+++ src/11_args.py
@@ -5,8 +5,11 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return int(x) + int(y)
 
-print(f1(1, 2))
+
+print('F1: ', f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
@@ -14,15 +17,20 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+
+def f2(*args):
+    return sum(args)
+
+
+print('F2 1: ', f2(1))                    # Should print 1
+print('F2 2: ', f2(1, 3))                 # Should print 4
+print('F2 3: ', f2(1, 4, -12))            # Should print -7
+print('F2 4: ', f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print('F2 5: ', f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -31,8 +39,16 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+
+def f3(arg1, arg2=None):
+    if arg2 == None:
+        return arg1 + 1
+    else:
+        return arg1 + arg2
+
+
+print('F3 1: ', f3(1, 2))  # Should print 3
+print('F3 2: ', f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -44,6 +60,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print('F4 using **kwargs (keyword values)->',
+              f"key: {key}, value: {value} ")
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +83,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
